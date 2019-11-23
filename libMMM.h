a8e156c70b5ee5fe7cb16c174c62ea30a7b3d39b
Jesper Christensen <jeschrper@hotmail.com>
2019-11-23 16:58:31 +0100

    Split to do DEC

--- libMMM.h
+++ libMMM.h
@@ -14,18 +14,22 @@
 
 const struct tCommand transformerCommands[] = {
     {"idl", &lib3m_idle               }, // 0x0: Idle
-    {"gtn", &lib3m_offsetB2           }, // 0x1: All channels transpose note (transposeBy)
-    {"gvo", &lib3m_offsetB3           }, // 0x2: All channels offset velocity (offsetBy)
-    {"gvs", &lib3m_setB3              }, // 0x3: All channels set velocity (value)
-    {"ctn", &lib3m_offsetB2IfChannel  }, // 0x4: Channel transpose note (channel, transposeBy)
-    {"cvo", &lib3m_offsetB3IfChannel  }, // 0x5: Channel offset velocity (channel, offsetBy)
-    {"cvs", &lib3m_setB3IfChannel     }, // 0x6: Channel set velocity (channel, value)
-    {"chs", &lib3m_setChannel         }, // 0x7: Set channel (channel)
-    {"chm", &lib3m_mapChannel         }, // 0x8: Map channel (channel, channel)
-    {"ccm", &lib3m_mapB2              }, // 0x9: Map CC (controller, controller)
-    {"pcm", &lib3m_mapB2              }, // 0xA: Map ProgChg (program, program)
-    {"evm", &lib3m_mapB1              }, // 0xB: Map Event (event, event)
-    {"lsp", &lib3m_splitKb            }  // 0xC: Split Notes (splitnote, channel, channel)
+    {"gtu", &lib3m_INCB2              }, // 0x1: All channels transpose note up (transposeBy)
+    {"gtd", &lib3m_DECB2              }, // 0x2: All channels transpose note down (transposeBy)
+    {"gvu", &lib3m_INCB3              }, // 0x3: All channels offset velocity up (offsetBy)
+    {"gvd", &lib3m_DECB3              }, // 0x4: All channels offset velocity down (offsetBy)
+    {"gvs", &lib3m_setB3              }, // 0x5: All channels set velocity (value)
+    {"ctu", &lib3m_INCB2IfChannel     }, // 0x6: Channel transpose note up (channel, transposeBy)
+    {"ctd", &lib3m_DECB2IfChannel     }, // 0x7: Channel transpose note down (channel, transposeBy)
+    {"cvu", &lib3m_INCB3IfChannel     }, // 0x8: Channel offset velocity up (channel, offsetBy)
+    {"cvd", &lib3m_DECB3IfChannel     }, // 0x9: Channel offset velocity down (channel, offsetBy)
+    {"cvs", &lib3m_setB3IfChannel     }, // 0xA: Channel set velocity (channel, value)
+    {"chs", &lib3m_setChannel         }, // 0xB: Set channel (channel)
+    {"chm", &lib3m_mapChannel         }, // 0xC: Map channel (channel, channel)
+    {"ccm", &lib3m_mapB2              }, // 0xD: Map CC (controller, controller)
+    {"pcm", &lib3m_mapB2              }, // 0xE: Map ProgChg (program, program)
+    {"evm", &lib3m_mapB1              }, // 0xF: Map Event (event, event)
+    {"lsp", &lib3m_splitKb            }  // 0x10: Split Notes (splitnote, channel, channel)
 };
 
 uint8_t decBigByte(uint8_t val, uint8_t decode){ 
--- libMMM_f.h
+++ libMMM_f.h
@@ -13,12 +13,20 @@ void lib3m_idle(midiPacket_t* pk, transformerParms_t tp){
 
 }
 
-void lib3m_offsetB2(midiPacket_t* pk, transformerParms_t tp){ 
+void lib3m_INCB2(midiPacket_t* pk, transformerParms_t tp){ 
     pb2 += tp.x;
 }
 
-void lib3m_offsetB3(midiPacket_t* pk, transformerParms_t tp){
+void lib3m_INCB3(midiPacket_t* pk, transformerParms_t tp){
     pb3 += tp.x;
+}
+
+void lib3m_DECB2(midiPacket_t* pk, transformerParms_t tp){ 
+    pb2 -= tp.x;
+}
+
+void lib3m_DECB3(midiPacket_t* pk, transformerParms_t tp){
+    pb3 -= tp.x;
 }
 
 void lib3m_setB3(midiPacket_t* pk, transformerParms_t tp){
@@ -41,12 +49,20 @@ void lib3m_mapChannel(midiPacket_t* pk, transformerParms_t tp){
     if (pchn == tp.x) pb1 = psts | tp.y;
 }
 
-void lib3m_offsetB2IfChannel(midiPacket_t* pk, transformerParms_t tp){
+void lib3m_INCB2IfChannel(midiPacket_t* pk, transformerParms_t tp){
     if (pchn == tp.x) pb2 += tp.y;
 }
 
-void lib3m_offsetB3IfChannel(midiPacket_t* pk, transformerParms_t tp){
+void lib3m_INCB3IfChannel(midiPacket_t* pk, transformerParms_t tp){
     if (pchn == tp.x) pb3 += tp.y;
+}
+
+void lib3m_DECB2IfChannel(midiPacket_t* pk, transformerParms_t tp){
+    if (pchn == tp.x) pb2 -= tp.y;
+}
+
+void lib3m_DECB3IfChannel(midiPacket_t* pk, transformerParms_t tp){
+    if (pchn == tp.x) pb3 -= tp.y;
 }
 
 void lib3m_setB3IfChannel(midiPacket_t* pk, transformerParms_t tp){
